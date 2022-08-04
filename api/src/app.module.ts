083a2bd72abe33afa08a18fed809fbaf62252fd4
Lylian PERSON-GAY <lperson-@e1r10p20.clusters.42paris.fr>
2022-08-04 13:27:12 +0200

    ADD: auth and user modules to app

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
