d9861893572c7e4ba38be10c72a9b8fbcd99cae3
lvirgini <lvirgini@student.42.fr>
2022-08-04 08:32:42 +0200

    SAVE: for school: images by theme in progress

--- app/src/App.vue
+++ app/src/App.vue
@@ -2,11 +2,18 @@
 import ChangeTheme from '@/components/changeTheme.vue'
 import AuthenticationPage from '@/view/AuthenticationPage.vue'
 
+//init theme: a refaire car reactualise la couleur au refresh 
+if (!document.documentElement.className)
+  document.documentElement.className = 'blue';
+
+// setTheme("green");
+
 </script>
 
 <template>
   <header>
     <ChangeTheme />
+
   </header>
 
   <main>
--- app/src/components/changeTheme.vue
+++ app/src/components/changeTheme.vue
@@ -1,7 +1,61 @@
 <script setup lang="ts">
+//     function reload(){
+//       var container = document.getElementById("thematic");
+//       var content = container.innerHTML;
+//       container.innerHTML= content; 
+      
+//      //this line is to watch the result in console , you can remove it later	
+//       console.log("Refreshed"); 
+//   }
+
+//   function img_reload_theme() {
+
+//     reload()
+//     // $( "#thematic" ).load(window.location.href + " #thematic" );
+//     //  document.getElementById('thematic').innerHTML = location.reload();
+//   }
+    //     img_reload_theme();
+    
+    //     // document.getElementsByName("img").forEach.apply();
+    //     // document.getElementById("img").contentWindow.location.reload(true);
+    //     // document.getElementById("youriframeid").contentWindow.location.reload(true);
+    //     //  var container = document.getElementById("thematic").contains.
+    //     //  var container = document.getElementById("thematic");
+    //     // var content = container.innerHTML;
+    //     //   container.innerHTML= ""; 
+    //     //   container.innerHTML= content; 
+          
+    //      //this line is to watch the result in console , you can remove it later	
+    //       console.log("Refreshed"); 
+        
+
   function setTheme(theme) {
     document.documentElement.className = theme;
-  } 
+    window.location.reload();
+ }
+
+
+
+
+  function get_images_by_theme(image: string, theme: string) {
+  
+    console.log(document.documentElement.className);
+    console.log("src/components/icons/" + image + '_' + document.documentElement.className + ".png");
+    return "src/components/icons/" + image + '_' + document.documentElement.className + ".png";
+    
+  }
+
+  // function get_images_by_theme(image: string, theme: string) {
+  
+  //   // console.log(document.getElementsByClassName("root"));
+  //   // console.log(localStorage.getItem("root"));
+  //   // console.log(document.documentElement.style.getPropertyValue());
+  //   console.log(document.documentElement.className);
+  //   // console.log("src/components/icons/" + image + '_' + theme + ".png");
+  //   return "src/components/icons/" + image + '_' + theme + ".png";
+    
+  // }
+
 
 </script>
 
@@ -18,4 +72,8 @@
         THEME YELLOW
       </button>
 	</div>
+
+  <img :src="get_images_by_theme('WhiskeyRose')" />
+  <!-- <img :src='src/components/icons/WhiskeyRose_{{document.documentElement.className}}.png' /> -->
+  <!-- <img :[src]="src/components/icons/WhiskeyRose_" + document.documentElement.className + ".png" /> -->
 </template>
