9a43af9d4b07915e4e45fa3a209bbbf2e82c388f
Tony <bbellavi@student.42.fr>
2022-08-18 17:57:09 +0200

    Add: PostgreSQL and TypeORM configuration

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,13 +2,29 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { JWTAuthGuard } from './auth/guards/jwtauth.guard';
+import { DataSource } from 'typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot(), AuthModule],
+  imports: [
+    ConfigModule.forRoot(), 
+    AuthModule,
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [],
+      // TODO: Remove for production
+      synchronize: true
+    })
+  ],
   controllers: [AppController],
   providers: [
     {
@@ -18,4 +34,6 @@ import { JWTAuthGuard } from './auth/guards/jwtauth.guard';
     AppService,
   ],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) { }
+}
