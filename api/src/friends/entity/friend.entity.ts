976887991d4c44f9677e9c8ef02d2584b8c2fa5d
Tony <bbellavi@student.42.fr>
2022-08-19 16:23:49 +0200

    Refactor: file architecture

---
+++ api/src/friends/entity/friend.entity.ts
@@ -0,0 +1,19 @@
+import { BaseEntity, Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { UserEntity } from "../../users/entities/user.entity"
+
+@Entity("friends")
+export class FriendEntity extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    friend_id: string;
+
+    @ManyToOne(() => UserEntity, (user) => user.friends)
+    @JoinColumn({ name: "user_1" })
+    user_1: UserEntity;
+
+    @ManyToOne(() => UserEntity, (user) => user.friend_of_other)
+    @JoinColumn({ name: "user_2" })
+    user_2: UserEntity;
+
+    @Column()
+    pending: boolean;
+}
