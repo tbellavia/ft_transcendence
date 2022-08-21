c6e4e70c8522621affbbb3e6760aa7cdf322a958
Tony BELLAVIA <bbellavi@e1r1p1.clusters.42paris.fr>
2022-08-21 14:17:02 +0200

    Remove: app service

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -1,11 +1,8 @@
 import { Controller, Get, Request } from '@nestjs/common';
-import { AppService } from './app.service';
 import { Public } from './common/decorators/public.decorator';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Public()
   @Get('')
   getIndex(): string {
--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -4,7 +4,6 @@ import { APP_GUARD } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { JWTAuthGuard } from './auth/guards/jwtauth.guard';
 import { DataSource } from 'typeorm';
@@ -45,7 +44,6 @@ import { UsersModule } from './users/users.module';
       provide: APP_GUARD,
       useClass: JWTAuthGuard,
     },
-    AppService,
   ],
 })
 export class AppModule {
