e1f2a2acaa522b0fcb3de0bbf7b7e415e9857e1b
lperson- <lperson-@student.42.fr>
2022-08-25 00:11:26 +0200

    ADD: cors credentatials authorization
    For sessions cookies

--- api/src/main.ts
+++ api/src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: true,
+    credentials: true
   });
   app.use(cookieParser());
   await app.listen(3000);
