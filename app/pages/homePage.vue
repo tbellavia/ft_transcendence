6fdc5561994fb71a82062461984d2b89be3f21c8
Lylian PERSON-GAY <lperson-@e1r3p21.clusters.42paris.fr>
2022-08-30 16:51:56 +0200

    ADD: qr-code fetching

--- app/pages/homePage.vue
+++ app/pages/homePage.vue
@@ -3,12 +3,32 @@
       <div style="width: 15%">
         <userpageUserList />
       </div>
-      <div style="width: 60%; border: solid; border-color: blue"></div>
-      <div style="width: 35%; border: solid; border-color: orange"></div>
+      <div style="width: 60%; border: solid; border-color: blue">
+      </div>
+      <div style="width: 35%; border: solid; border-color: orange; color: var(--main-color-light)">
+        <div v-html="svg.outerHTML"></div>
+        <!-- <div v-html="temp.innerHTML"></div> -->
+      </div>
   </div>
 </template>
 
-<script></script>
+<script setup>
+  const { $apiFetch } = useNuxtApp();
+
+  const qrCode = await $apiFetch('/2fa/generate', {
+    method: 'POST'
+  });
+
+  const temp = document.createElement('temporary');
+  temp.innerHTML = qrCode.trim();
+  const svg = temp.firstChild;
+
+  for (let i = 0; i < svg.children.length; ++i) {
+    svg.children[i].setAttribute('stroke', 'currentColor');
+    svg.children[i].setAttribute('fill', 'none');
+  }
+  console.log(svg);
+</script>
 
 <style>
 .home-page {
