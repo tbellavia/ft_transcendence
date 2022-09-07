3f07a13ae86dffb6dedee17bf74f32efce670838
Lylian PERSON-GAY <lperson-@e2r2p17.clusters.42paris.fr>
2022-09-07 15:18:10 +0200

    ADD: chat compoments

---
+++ app/components/chat/display.vue
@@ -0,0 +1,36 @@
+<template>
+  <!-- Displaying chat messages area -->
+  <ul class="display-chat">
+    <li v-for="message in messages">
+      <p>
+        <span class="author">{{ message.from }}</span>
+        {{ message.message }}
+      </p>
+    </li>
+  </ul>
+
+  <!-- Send message -->
+  <form action="#" submit="sendMessage">
+    <input type="text" />
+  </form>
+</template>
+
+<script lang="ts" setup>
+// Target is the name of the discussion (username or channel)
+// isChannel control if the name target a channel discussion
+defineProps({
+  isChannel: Boolean,
+  target: {
+    type: String,
+    required: true
+  },
+});
+
+// Interface and ref for displaying messages
+interface chatMessage {
+  message: string;
+  from: string;
+};
+const messages = ref<chatMessage[]>([]);
+
+</script>
---
+++ app/components/chat/send.vue
@@ -0,0 +1,28 @@
+<template>
+  <form action="" @submit.prevent="">
+      <input type="text" placeholder="message"  v-model="message"/>
+      <input type="submit" />
+  </form>
+</template>
+
+<script lang="ts" setup>
+// Target is the name of the discussion (username or channel)
+// isChannel control if the name target a channel discussion
+defineProps({
+  isChannel: Boolean,
+  target: {
+    type: String,
+    required: true
+  },
+});
+
+// Interface and refs for sending messages
+interface sendMessage {
+  message: string;
+  target: string;
+  isChannel: boolean;
+}
+
+const message = ref<string | undefined>();
+
+</script>
