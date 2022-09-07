db52ec404f572f582e68cd4e8004c05b07a10bbe
Lylian PERSON-GAY <lperson-@e2r2p17.clusters.42paris.fr>
2022-09-07 13:43:56 +0200

    CHANGE: desactivate 2auth verif in jwt strategy

--- api/src/auth/strategies/jwt.strategy.ts
+++ api/src/auth/strategies/jwt.strategy.ts
@@ -26,9 +26,12 @@ export class JWTStrategy extends PassportStrategy(Strategy, 'jwt') {
   // Populate req.user with user's entity
   async validate(payload: TokenPayload) {
     const user = await this.userService.findOneById(payload.uuid);
+    /*
     if (!user.is_two_factor_auth_enabled)
       return user;
     if (payload.isTwoFactorAuthenticated)
       return user;
+    */
+    return user;
   }
 }
