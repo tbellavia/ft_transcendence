ba185ff12a3a530a65a9cd5c4cc46e105e6be345
bbellavi <bbellavi@student42.fr>
2022-09-12 12:14:59 +0200

    Add: @Expose() on stats fields

--- api/src/stats/entities/stat.entity.ts
+++ api/src/stats/entities/stat.entity.ts
@@ -1,5 +1,6 @@
 import { UserEntity } from "../../users/entities/user.entity";
 import { Column, PrimaryGeneratedColumn, Entity, BaseEntity, OneToOne, JoinColumn } from "typeorm";
+import { Expose } from "class-transformer";
 
 export enum RankEnum {
     GOLD    = "Gold",
@@ -10,18 +11,23 @@ export enum RankEnum {
 
 @Entity("stats")
 export class StatEntity extends BaseEntity {
+    @Expose()
     @PrimaryGeneratedColumn("uuid")
     stat_id: string;
-
+    
+    @Expose()
     @Column({ nullable: true, default: 0 })
     game_total: number;
 
+    @Expose()
     @Column({ nullable: true, default: 0 })
     game_won: number;
 
+    @Expose()
     @Column({ nullable: true, default: 0 })
     game_abandonned: number;
 
+    @Expose()
     @Column({
         type: "enum",
         enum: RankEnum,
