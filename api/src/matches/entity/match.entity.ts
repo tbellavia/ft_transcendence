7d7ceb45970fad801ab11781d32c412b19f624d5
bbellavi <bbellavi@student42.fr>
2022-09-12 13:30:11 +0200

    Add: @Expose() on match entity fields

--- api/src/matches/entity/match.entity.ts
+++ api/src/matches/entity/match.entity.ts
@@ -1,3 +1,4 @@
+import { Expose } from "class-transformer";
 import { UserEntity } from "src/users/entities/user.entity";
 import { BaseEntity, Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 
@@ -9,23 +10,29 @@ export enum MatchOutcomeEnum {
 
 @Entity("matches")
 export class MatchEntity extends BaseEntity {
+    @Expose()
     @PrimaryGeneratedColumn("uuid")
     match_id: string;
 
+    @Expose()
     @ManyToOne(() => UserEntity, (user) => user.matches)
     @JoinColumn({ name: "user_1" })
     user_1: UserEntity;
 
+    @Expose()
     @ManyToOne(() => UserEntity, (user) => user.opponents)
     @JoinColumn({ name: "user_2" })
     user_2: UserEntity;
 
+    @Expose()
     @Column({ nullable: true })
     player_1_point: number;
 
+    @Expose()
     @Column({ nullable: true })
     player_2_point: number;
 
+    @Expose()
     @Column({
         type: "enum",
         enum: MatchOutcomeEnum,
@@ -33,6 +40,7 @@ export class MatchEntity extends BaseEntity {
     })
     player_1_outcome: MatchOutcomeEnum;
 
+    @Expose()
     @Column({
         type: "enum",
         enum: MatchOutcomeEnum,
@@ -40,12 +48,15 @@ export class MatchEntity extends BaseEntity {
     })
     player_2_outcome: MatchOutcomeEnum;
 
+    @Expose()
     @Column()
     begin_date: Date;
 
+    @Expose()
     @Column({ nullable: true })
     end_date: Date;
 
+    @Expose()
     @CreateDateColumn()
     creation_date: Date;
 }
