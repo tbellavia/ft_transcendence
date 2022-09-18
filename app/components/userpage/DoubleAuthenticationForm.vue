bfd502af0db53816d8e66f516c87ec107992552c
lvirgini <lvirgini@student.42.fr>
2022-09-18 16:29:54 +0200

    FIX: otp form for double authentication key

--- app/components/userpage/DoubleAuthenticationForm.vue
+++ app/components/userpage/DoubleAuthenticationForm.vue
@@ -1,22 +1,80 @@
 
 <template>
 	<div class="form">
-		<form method="get" class="digit-group" data-group-name="digits" data-autosubmit="false" autocomplete="off">
-			<input onkeyup="nextInput(2)" class="inputOtp" type="text" id="digit-1" name="digit-1" data-next="digit-2" placeholder="-" />
-			<input onkeyup="nextInput(3)" class="inputOtp" type="text" id="digit-2" name="digit-2" data-next="digit-3" data-previous="digit-1" placeholder="-" />
-			<input onkeyup="nextInput(4)" class="inputOtp" type="text" id="digit-3" name="digit-3" data-next="digit-4" data-previous="digit-2" placeholder="-" />
-			<input onkeyup="nextInput(5)" class="inputOtp" type="text" id="digit-4" name="digit-4" data-next="digit-5" data-previous="digit-3" placeholder="-" />
-			<input onkeyup="nextInput(6)" class="inputOtp" type="text" id="digit-5" name="digit-5" data-next="digit-6" data-previous="digit-4" placeholder="-" />
-			<input class="inputOtp" type="text" id="digit-6" name="digit-6" data-previous="digit-5" placeholder="-" />
+		<form  method="get" class="digit-group" data-group-name="digits" data-autosubmit="false">
+			<input v-model="input[0]" maxlength="1" @keyup="clickEvent('0', '1', $event)" class="inputOtp" type="text" id="0" placeholder="-" />
+			<input v-model="input[1]" maxlength="1" @keyup="clickEvent('1', '2', $event)" class="inputOtp" type="text" id="1" placeholder="-" />
+			<input v-model="input[2]" maxlength="1" @keyup="clickEvent('2', '3', $event)" class="inputOtp" type="text" id="2" placeholder="-" />
+			<input v-model="input[3]" maxlength="1" @keyup="clickEvent('3', '4', $event)" class="inputOtp" type="text" id="3" placeholder="-" />
+			<input v-model="input[4]" maxlength="1" @keyup="clickEvent('4', '5', $event)" class="inputOtp" type="text" id="4" placeholder="-" />
+			<input v-model="input[5]" maxlength="1" @keyup="clickEvent('5', '', $event)" class="inputOtp" type="text" id="5" placeholder="-" />
 		</form>
 		<v-space></v-space>
 		<button @click="checkKey"> validate </button>
 	</div>
 </template>
 
+<script setup lang="ts">
+	let input = ref([]);
+
+function clickEvent(current_id: string, next_id: string, event) {
+
+	console.log(input.value[Number(current_id)]);
+	console.log(input.value);
+	
+	// event.key we don't want to pass to another input
+	if (event.key === "Shift" || event.key === "Tab" || event.key === "Backspace") {
+		// if backspace and actual input empty return to before input
+		if (event.key === "Backspace" && !input.value[Number(current_id)]) {
+			if (current_id !== '0')
+			document.getElementById((Number(current_id) - 1).toString()).focus();
+		}
+		return;
+	}
+
+	else if (event.key === "Enter")
+		checkKey();
+
+	// pass to next_id input only if input is a number else reset value
+	else if ( (event.keyCode >= 97 && event.keyCode <= 105) 
+	|| (event.keyCode >= 48 && event.keyCode <= 57) 
+	&& next_id) {
+		document.getElementById(next_id).focus();
+	}
+	else
+		input.value[Number(current_id)] = '';
+}
+
+const emit = defineEmits({
+DoubleAuthValidate: null,
+})
+
+function checkKey() {
+	// LYLIAN: YOU CAN CHOOSE string or numbers:
+	// join() make a string of all number
+	console.log(input.value.join(''));
+
+	// you can Number(input.value.join(''))
+	console.log(Number(input.value.join('')));
+
+	if (input.value.join() === "123456")
+		emit("DoubleAuthValidate")
+	else {
+		// clean input and focus to first element input
+		input.value = [];
+		document.getElementById('0').focus();
+	}
+}
+	
+
+</script>
+
+
+
+
+
+
 <style scoped>
-
-
 .digit-group input {
 		width: 30px;
 		height: 40px;
@@ -54,51 +112,5 @@
 	}
 	
 </style>
-<script setup lang="ts">
-  let allowSpaces = ref( false);
-  let match = ref('coucou');
-  let max = ref(6);
-  let model = ref('');
 
-  const emit = defineEmits({
-  DoubleAuthValidate: null,
-})
-
-function nextInput(number: string) {
-	
-}
-
-//   watch(model,(newModel) => {
-// 	console.log(model);
-// 	if (model == '123456')
-// 		emit("DoubleAuthValidate")
-// 	else {
-// 		model.value = 0;
-// 		// tell some error
-// 	}
-// })
-
-function checkKey() {
-	console.log(model.value);
-	if (model.value)
-		emit("DoubleAuthValidate")
-	else {
-		model.value = "";
-		// tell some error
-	}
-}
-
-function DoubleAuthenticationFormRules() {
-	const rules = []
-	if (this.max) {
-		const rule =
-			v => (v || '').length <= this.max ||
-			`A maximum of ${this.max} characters is allowed`
-
-		rules.push(rule)
-	}
-	return rules
-}
-
-</script>
 
