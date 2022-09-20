f4ad119c1e27255c550e1f9c2d9c0bc4ea522940
Lylian PERSON-GAY <lperson-@e1r6p3.clusters.42paris.fr>
2022-09-20 16:04:24 +0200

    ADD: leave channel logic

---
+++ app/components/chat/channel/Parameters.vue
@@ -0,0 +1,36 @@
+<template>
+  <fieldset>
+    <button @click="leaveChannel">Leave Channel</button>
+  </fieldset>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  target: {
+    required: true,
+    type: String
+  }
+});
+
+const route = useRoute();
+const socket = useSocketChat();
+function leaveChannel() {
+  socket.value.emit(
+    'leave_channel',
+    props.target
+  );
+}
+
+const user = await useGetUser();
+
+// When user leave channel if it's currently load channel, load the main chat page
+socket.value.on(
+  'receive_leave_channel',
+  async ({username, channel}) => {
+    console.log('LEAVE CHANNEL: ', username, ':', channel.name);
+    if (user.value.username == username && channel.name == props.target)
+      await navigateTo(route.fullPath.slice(0, route.fullPath.lastIndexOf('/')));
+  }
+)
+
+</script>
