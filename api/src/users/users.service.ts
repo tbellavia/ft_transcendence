4bd067dc3c56c489cd6bc276f4d0cbb09d2fc124
Lylian PERSON-GAY <lperson-@e1r12p5.clusters.42paris.fr>
2022-09-26 18:11:15 +0200

    FIX: disable 2auth when regenerate secret that is not validated

--- api/src/users/users.service.ts
+++ api/src/users/users.service.ts
@@ -80,6 +80,7 @@ export class UsersService {
   async updateTwoFactorSecret(two_factor_secret: string, user_id: string) {
     const user = await this.findOneById(user_id);
     user.two_factor_auth_secret = two_factor_secret;
+    user.double_auth_enabled = false;
     await user.save();
   }
 
