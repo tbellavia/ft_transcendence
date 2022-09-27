421e16c06a674c1e3e2b98dcca08181efcb36d76
Lylian PERSON-GAY <lperson-@e1r11p4.clusters.42paris.fr>
2022-09-27 15:07:35 +0200

    ADD: list invite channels for users

--- app/components/chat/channel/List.vue
+++ app/components/chat/channel/List.vue
@@ -1,11 +1,19 @@
 <template>
   <div class="channels-component">
     <ChatChannelJoinOrCreate />
+    <h3>Channels joined</h3>
     <ul class="list-channels">
-      <li v-for="channel in channels">
+      <li v-for="channel in channelsJoined">
         <NuxtLink :to="`/${authUser.username}/chat/${channel}?isChannel`">
           <div class="channel-item">{{ channel }}</div>
         </NuxtLink>
+      </li>
+    </ul>
+    <h3>Channels invitations</h3>
+    <input type="password" placeholder="channel password" />
+    <ul class="list-channels">
+      <li v-for="channel in channelsInvited">
+        <button @click="acceptInvite(channel)">{{ channel }}</button>
       </li>
     </ul>
   </div>
@@ -13,24 +21,40 @@
 
 <script setup lang="ts">
 
-const channels = ref<string[]>([]);
+const channelsJoined = ref<string[]>([]);
+const channelsInvited = ref<string[]>([]);
 const authUser = await useGetUser();
 const socket = useSocketChat();
 
-//fetch all existing channels
+//fetch all channels joined
 socket.value.emit('get_all_channels_joined', {}, (channelsFetched: string[]) => {
-  channels.value = channels.value.concat(channelsFetched);
+  channelsJoined.value = channelsJoined.value.concat(channelsFetched);
+})
+
+socket.value.emit('get_all_channels_joined', {}, (channelsFetched: string[]) => {
+  channelsInvited.value = channelsInvited.value.concat(channelsFetched);
 })
 
 //watch joining or creating channel events
 socket.value.on('receive_create_channel', (channel: string) => {
-  channels.value.push(channel);
+  channelsJoined.value.push(channel);
 });
 
 socket.value.on('receive_join_channel', ({ username, channelName }: {username: string, channelName: string}) => {
   if (username == authUser.value.username)
-    channels.value.push(channelName);
+    channelsJoined.value.push(channelName);
 });
+
+socket.value.on('receive_invite_channel', (channel: string) => channelsInvited.value.push(channel));
+const channelPassword = ref('');
+function acceptInvite(channel: string) {
+  socket.value.emit('join_channel', {
+    name: channel,
+    password: channelPassword
+    },
+    () => channelsInvited.value.splice(channelsInvited.value.findIndex(channelName => channelName == channel), 1)
+  );
+}
 
 </script>
 
